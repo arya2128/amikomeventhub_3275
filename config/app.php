24ce292ad6e067c59fa2057ca83198055f4a16dc
arya2128 <aryaradhyaksa@students.amikom.ac.id>
2026-07-30 04:36:24 +0700

    Fix Invalid URI crash during build by validating APP_URL fallback

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => (!env('APP_URL') || env('APP_URL') === 'https://' || !filter_var(env('APP_URL'), FILTER_VALIDATE_URL)) ? 'http://localhost' : env('APP_URL'),
 
     /*
     |--------------------------------------------------------------------------
